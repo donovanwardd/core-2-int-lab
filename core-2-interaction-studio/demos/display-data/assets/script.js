6a2decb971eab5198b21a1914ef230ce732f5a32
donovanwardd <wardd319@newschool.edu>
2023-02-28 21:40:20 -0500

    added json example demo

---
+++ core-2-interaction-studio/demos/display-data/assets/script.js
@@ -0,0 +1,51 @@
+//setup container element
+let container = document.getElementById("container");
+
+let emojiObject = {
+  "by": "Emily",
+  "id": 1,
+  "emoji": "🌄",
+  "img": "filename-img.png",
+  "description": "Sunrise over mountains",
+  "alt-meaning": "Sun, Mountains, Early Morning",
+  "category": "Travel & Places",
+  "year": "October 2010",
+  "update": "Version 6.0",
+  "general-usage": "4 (Yearly Rankn 389)",
+  "personal-usage": 1,
+  "intensity": 2,
+  "sample": "🔸 The sunrise over the mountains🌄 was a charming scene.",
+  "unicode": "U+1F304"
+}
+
+
+
+
+// must setup a local server to use fetch
+// see Python instructions here:
+// https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server#using_python
+
+fetch('./assets/emojis.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    displayData(data);
+  })
+  .catch(error => console.log(error));
+
+function processEmojis( data ){
+  data.forEach( function(item, index){
+    console.log(item, index);
+    let usgae = item['general-usage'];
+    console.log('usgae', usage);
+    let newItem = document.createElement("div");
+    newItem.classList.add("icon");
+    //newItem.style.cssText = 'font-size: ${usage}px';
+    newItem.innerHTML = `
+      <div class="usage">$[item{'general-usage'}]</div>
+      <div class="phrase">${item.sample}</div>
+      <div class="category">${item.category}</div>
+      <div class="emoji">${item.emoji}</div>`;
+    container.appendChild(newItem);    
+  });
+}
